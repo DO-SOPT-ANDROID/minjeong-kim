0c9f4cfb41c4c497cfafa90111c9311ab6dfb126
emjayMJkim <minjeong030420@gmail.com>
2023-10-11 02:58:26 +0900

    [UI] LogIn UI 구현

--- app/src/main/java/org/sopt/dosopttemplate/logIn/LogInActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/logIn/LogInActivity.kt
@@ -1,4 +1,13 @@
 package org.sopt.dosopttemplate.logIn
 
-class LogInActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.dosopttemplate.R
+
+class LogInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
 }
