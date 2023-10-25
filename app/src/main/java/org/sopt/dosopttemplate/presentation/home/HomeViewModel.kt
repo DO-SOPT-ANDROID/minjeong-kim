a2a3eac974f89b61f08339c6d1dd54c71afe9bcb
emjayMJkim <minjeong030420@gmail.com>
2023-10-26 00:11:30 +0900

    [MOD] 데이터 리스트 수정

--- app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeViewModel.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeViewModel.kt
@@ -19,8 +19,8 @@ class HomeViewModel : ViewModel() {
         Profile.FriendIncludeMusic(
             "https://www.ghibli.jp/gallery/karigurashi046.jpg",
             "아리에띠",
-            "남주랑",
-            "마지막처럼-블랙핑크"
+            "남주와 마지막 인사",
+            "마루밑아리에띠"
         ),
         Profile.FriendOriginal(
             "https://www.ghibli.jp/gallery/karigurashi018.jpg",
@@ -31,10 +31,42 @@ class HomeViewModel : ViewModel() {
             "https://www.ghibli.jp/gallery/karigurashi014.jpg",
             "아리에띠",
             "놀라는 아리에띠"
-        )
+        ),
+        Profile.FriendIncludeMusic(
+            "https://www.ghibli.jp/gallery/ponyo028.jpg",
+            "포뇨",
+            "달리는 포뇨",
+            "벼랑 위의 포뇨"
+        ),
+        Profile.FriendOriginal(
+            "https://www.ghibli.jp/gallery/ponyo004.jpg",
+            "포뇨",
+            "귀여워.."
+        ),
+        Profile.FriendOriginal(
+            "https://www.ghibli.jp/gallery/chihiro014.jpg",
+            "검댕이",
+            "센과 치히로의 행방불명"
+        ),
+        Profile.FriendOriginal(
+            "https://www.ghibli.jp/gallery/majo008.jpg",
+            "키키",
+            "마녀배달부 키키"
+        ),
+        Profile.FriendOriginal(
+            "https://www.ghibli.jp/gallery/howl012.jpg",
+            "캘시퍼",
+            "하울의 움직이는 성"
+        ),
+        Profile.FriendIncludeMusic(
+            "https://www.ghibli.jp/gallery/totoro044.jpg",
+            "토토로",
+            "냥",
+            "이웃집 토토로"
+        ),
     )
 
     val mockProfileList : List<Profile>
         get() = _mockProfileList
-    //fun getProfileList() = _mockProfileList
+
 }
