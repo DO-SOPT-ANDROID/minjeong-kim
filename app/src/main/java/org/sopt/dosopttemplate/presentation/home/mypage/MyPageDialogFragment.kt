8ae1d772bc4b2c598000e72bb7cac3e9759a2b88
emjayMJkim <minjeong030420@gmail.com>
2023-11-10 11:47:41 +0900

    [FEAT] logOut Dialog

---
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/home/mypage/MyPageDialogFragment.kt
@@ -0,0 +1,50 @@
+package org.sopt.dosopttemplate.presentation.home.mypage
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.base.BaseDialogFragment
+import org.sopt.dosopttemplate.data.datasource.local.SharedPreference
+import org.sopt.dosopttemplate.databinding.FragmentMypageDialogBinding
+import org.sopt.dosopttemplate.presentation.logIn.LogInActivity
+
+class MyPageDialogFragment : BaseDialogFragment<FragmentMypageDialogBinding>() {
+
+    override val layoutResId: Int
+        get() = R.layout.fragment_mypage_dialog
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initMakeDialogCancelable()
+        initSetCancleBtn()
+        initSetConfirmBtn()
+    }
+
+    private fun initMakeDialogCancelable() {
+        dialog?.setCancelable(true)
+        dialog?.setCanceledOnTouchOutside(true)
+    }
+
+    private fun initSetCancleBtn() {
+        binding.tvDialogCancel.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    private fun initSetConfirmBtn() {
+        binding.tvDialogConfirm.setOnClickListener {
+            setLogOut()
+        }
+    }
+
+    private fun setLogOut() {
+        SharedPreference.clearUserData()
+        activity?.let {
+            val intent = Intent(context, LogInActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
+
